5a05845fddd285d94e3f4595fe3fe642dfd5ba01
Bruce Alderson <balderson@discoverysoftware.com>
2013-05-20 20:56:26 -0700

    Shell HTML.

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+	<title>Roshambonator v1</title>
+	<meta name="description" content="A simple Roshambo AI shim." />
+  	<meta name="keywords" content="Roshambo, AI, contest, UOA, Alberta, Napkinware" />
+
+	<script type="text/javascript" src="http://use.typekit.com/kku6vpo.js"></script>
+	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+
+	<link rel="stylesheet" href="/theme/roshambo.less" />
+	<script src="/roshambonator.js" type="text/javascript"></script>
+
+<!--[if lt IE 9]>
+	<script src="//html5shiv.googlecode.com/svn/trunk/html5.js" onload="window.ieshiv=true;"></script>
+<![endif]-->
+
+</head>
+<?php flush(); ?><body>
+<header><div>
+	<h1><a href="/">Roshambinator <em>Rock? Paper? Scissors?</em></a></h1>
+<div></header>
+
+
+<main><div>
+</div></main>
+
+<footer><div>
+	<p><a href="/"><?= $chronicle->settings->site->name ?></a> &mdash; COPYRIGHT &copy; 1997-<?= date('Y'); ?> BRUCE ALDERSON</p>
+</div></footer>
+
+</body>
+</html>
